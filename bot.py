2a2a52afd68440b1b8ab909b53b100f8a267c4c8
dayan03 <72884237+dayan03@users.noreply.github.com>
2026-06-09 19:41:34 -0500

    git commit -m "Corregir servidor web ping 02"

--- bot.py
+++ bot.py
@@ -394,12 +394,18 @@ class PingHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-Length', '14')
         self.end_headers()
         self.wfile.write(b"Bot corriendo OK")
     
     def do_HEAD(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-Length', '14')
+        self.end_headers()
+
+    def do_POST(self):
+        self.send_response(200)
         self.end_headers()
 
     def log_message(self, format, *args):
